14ff5b6f9a008b8f546ca2dc30c9bcc17299bb06
Chrissy Weeks <fevrin@gmail.com>
2014-11-07 18:26:22 -0500

    clock-reminder.rb * updated schedule + added handling in methods when schedule is nil + added ability to specify time for get_time() + added a fun little spinner method + added timeline printing that outputs start, current, and end times   according to their relative distance between each other

--- clock-reminder.rb
+++ clock-reminder.rb
@@ -14,8 +14,8 @@ SCHEDULE = {
    "tuesday" => {"start" => "15:00", "end" => "23:00"},
    "wednesday" => {"start" => "15:00", "end" => "23:00"},
    "thursday" => {"start" => "15:00", "end" => "23:00"},
-   "friday" => {"start" => "15:00", "end" => "23:00"},
-   "saturday" => {"start" => "15:00", "end" => "23:00"}
+   "friday" => {"start" => nil, "end" => nil},
+   "saturday" => {"start" => nil, "end" => nil}
 }
 VERBOSE=2
 
@@ -27,16 +27,18 @@ def get_day()
    %x(date +%A).chomp
 end
 
-def get_time(format = "human")
-   time = Time.now
-
-   case format
-      when "human"
-         format = "%H:%M"
-         return time.strftime("#{format}")
-      when "epoch"
-         format = "%s"
-         return time.strftime("#{format}").to_i
+def get_time(format = "human", time = Time.now)
+   if (time.nil?) then
+      return nil
+   else
+      case format
+         when "human"
+            format = "%H:%M"
+            return time.strftime("#{format}")
+         when "epoch"
+            format = "%s"
+            return time.strftime("#{format}").to_i
+      end
    end
 end
 
@@ -48,35 +50,66 @@ end
 
 def get_shift_time(time)
    if (time =~ /(start|end)/) then
-      return Time.parse(SCHEDULE[get_day.downcase][time].to_s).strftime("%s").to_i
+      given_time = SCHEDULE[get_day.downcase][time]
+      if (given_time.nil?) then
+         return nil
+      else
+         return get_time("epoch", Time.parse(given_time))
+      end
    else
       return nil
    end
 end
 
-verbose(1, "it is #{get_day()} at #{get_time("epoch").to_s} (#{get_time("human")})!")
-verbose(2, "compare that to " + (get_shift_time("start") + 300).to_s + "!")
+def spinner()
+#thx to <http://stackoverflow.com/a/7366089>
+   while(true) do
+     print "\\\r"
+     print "|\r"
+     print "/\r"
+   end
+end
 
 def compare_clock_status()
 #check if it's been more than X minutes since my shift started or ended
 
    compare_time_start = get_shift_time("start")
    compare_time_end = get_shift_time("end")
+   current_time = get_time("epoch")
 
-   verbose(1, get_time("epoch").to_s + " > " + (compare_time_start + 300).to_s + "; status = " + get_clock_status)
-
-   if get_time("epoch") >= compare_time_start && get_clock_status == "in" then
-      message="yay, you clocked in on time!"
-   elsif get_time("epoch") >= compare_time_end && get_clock_status == "out" then
-      message="yay, you clocked out on time!"
-   elsif get_time("epoch") > (compare_time_start + 300) && get_clock_status == "out" then
-      message="don't forget to clock in!"
-   elsif get_time("epoch") > (compare_time_end + 300) && get_clock_status == "in" then
-      message="don't forget to clock out!"
+   if (compare_time_start.nil? || compare_time_end.nil?) then
+      return nil
    else
-      message="what are you doing right now, babe?"
+      verbose(1, get_time("human") + " > " + get_time("human",Time.at(compare_time_start + 300)) + "; status = " + get_clock_status)
+      verbose(1, get_time("human") + " > " + get_time("human",Time.at(compare_time_end + 300)) + "; status = " + get_clock_status)
+   
+      # print out a timeline of where we are compared to the start and end of the shift
+      timeline = [compare_time_start, compare_time_end, current_time].sort
+
+      # convert the difference between start, current, and end times to delimiters
+      delimiter_a = (timeline[0] - timeline[1]).abs
+      delimiter_a = (delimiter_a /= 1800).round.to_s
+      delimiter_a = ("." * delimiter_a.to_i).to_s
+
+      # same as above, just on one line :)
+      delimiter_b = ("." * ((((timeline[1] - timeline[2]).abs) / 1800).round.to_s).to_i).to_s
+
+      timeline.each_index { |a| timeline[a] = get_time("human", Time.at(timeline[a])) }
+      verbose(1, timeline[0] + delimiter_a + timeline[1] + delimiter_b + timeline[2])
+   
+      if current_time >= compare_time_start && get_clock_status == "in" then
+         message="yay, you clocked in!"
+      elsif current_time >= compare_time_end && get_clock_status == "out" then
+         message="yay, you clocked out!"
+      elsif current_time > (compare_time_start + 300) && get_clock_status == "out" then
+         message="don't forget to clock in!"
+      elsif current_time > (compare_time_end + 300) && get_clock_status == "in" then
+         message="don't forget to clock out!"
+      else
+         message="what are you doing right now, babe?"
+      end
+      return message
    end
-   return message
 end
 
 def slack_it(message)
@@ -87,7 +120,14 @@ end
 #check if I have clocked in or out, as appropriate
 message = compare_clock_status()
 
-verbose(1, message)
+if (message.nil?) then
+   puts "you don't work today!"
+else
+   verbose(1, "it is #{get_day()} at #{get_time("epoch").to_s} (#{get_time("human")})!")
+   verbose(2, "compare that to " + (get_shift_time("start").to_i + 300).to_s + " (" + get_time("human",Time.at((get_shift_time("start").to_i + 300))) + ") - " + (get_shift_time("end").to_i + 300).to_s + " (" + get_time("human",Time.at((get_shift_time("end").to_i + 300))) + ")!")
 
-#post the message to the specified channel
-slack_it(message)
+   #post the message to the specified channel
+   slack_it(message)
+
+   verbose(1, message)
+end
